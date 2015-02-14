134a8bf678dd46236b9c9a5f836474f3de02cb78
laurenpawell <lauren.pawell@gmail.com>
2015-02-14 10:00:25 -0800

    Added user association to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214174438) do
+ActiveRecord::Schema.define(version: 20150214175421) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "post_title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
