dd15b4db2b721adac7234c1519324ba4de400b8a
laurenpawell <lauren.pawell@gmail.com>
2015-02-13 17:01:59 -0800

    Added show, edit & update views and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -20,17 +21,28 @@ class PostsController < ApplicationController
     end
   end
 
-  private
-    def post_params
-        params.require(:post).permit(:post_title, :content)
-    end
-
-  def update
-  end
-
   def edit
   end
 
-  def delete
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit(:post_title, :content)
+    end
 end
