9cd96f802e83dfa5c171c63082e45f17df8608dd
laurenpawell <lauren.pawell@gmail.com>
2015-02-13 16:26:46 -0800

    Created post show view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,28 @@ class PostsController < ApplicationController
   def index
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
-  def show
-  end
+  private
+    def post_params
+        params.require(:post).permit(:post_title, :content)
+    end
 
   def update
   end
