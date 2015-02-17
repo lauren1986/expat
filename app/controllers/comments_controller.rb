56e1755214fd34d63f039dcb327fbc9d43c8291a
laurenpawell <lauren.pawell@gmail.com>
2015-02-17 00:20:39 -0800

    Create, edit, delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
+    @comment.user_id = current_user.id if current_user
 
     if @comment.save
       redirect_to post_path(@post)
@@ -9,5 +10,30 @@ class CommentsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.update(params[:comment].permit(:comment))
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
 
 end
