583215429356a93792412dcaf61c5b76b15506a9
laurenpawell <lauren.pawell@gmail.com>
2015-02-17 19:38:45 -0800

    Added first name, last name and bio fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217070003) do
+ActiveRecord::Schema.define(version: 20150218031343) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20150217070003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
