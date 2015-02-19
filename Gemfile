a5cd84ccb7fd96a048a02aa728a2100d6b50bbf3
laurenpawell <lauren.pawell@gmail.com>
2015-02-18 17:49:10 -0800

    Started adding FB OmniAuth through devise

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem "breadcrumbs_on_rails"
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
+# Facebook OmniAuth
+gem 'omniauth-facebook'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218031343) do
+ActiveRecord::Schema.define(version: 20150219014430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20150218031343) do
     t.string   "first_name"
     t.string   "last_name"
     t.text     "bio"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
