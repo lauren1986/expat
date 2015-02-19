0ae0824ae45da846bace45347b559b6f72ad9da0
laurenpawell <lauren.pawell@gmail.com>
2015-02-18 17:43:29 -0800

    Add extra fields to devise user

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+   def sign_up_params
+     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio)
+   end
+
+   def account_update_params
+     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :bio)
+   end
+
+end
