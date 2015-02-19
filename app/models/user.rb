12f7bb686deb37f0537b9a7cb414aff4910d3fbb
laurenpawell <lauren.pawell@gmail.com>
2015-02-18 18:22:03 -0800

    Messed up FB OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   has_many :posts
   has_many :comments
+
 end
