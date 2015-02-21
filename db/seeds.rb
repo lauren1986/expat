41785c4d3b2b5c27a64ca89f76fe8f42d630418a
laurenpawell <lauren.pawell@gmail.com>
2015-02-20 17:15:37 -0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    categories = Category.create([{ category_name: 'Employment' }, { category_name: 'Health & Safety' }, { category_name: 'Finance' }, { category_name: 'Housing' }, { category_name: 'Immigration & Visas' }, { category_name: 'Language & Education' }, { category_name: 'Miscellaneous' }, { category_name: 'People & Culture' },
+      { category_name: 'Things To Do' }, { category_name: 'Transporation' }])
