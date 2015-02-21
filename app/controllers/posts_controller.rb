2b8fb3d1a34dd0b5ad08afb35ae29701810e3bb4
laurenpawell <lauren.pawell@gmail.com>
2015-02-21 10:26:46 -0800

    So many errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    @posts = Post.paginate(:page => params[:page], :per_page => 8, :order => 'created_at DESC')
+    @posts = Post.order('created_at DESC').paginate(:page => params[:page], :per_page => 8)
     add_breadcrumb "index", root_path
   end
 
