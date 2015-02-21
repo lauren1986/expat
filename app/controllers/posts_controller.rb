a27605c571f3224748bdaddd50bf7f96133cf5e9
laurenpawell <lauren.pawell@gmail.com>
2015-02-21 10:20:19 -0800

    ASC vs DESC

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   add_breadcrumb "Category", :root_path
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.all.order("created_at ASC")
     @posts = Post.paginate(:page => params[:page], :per_page => 8)
     add_breadcrumb "index", root_path
   end
