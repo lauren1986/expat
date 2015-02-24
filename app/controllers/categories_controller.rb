4e1172b5c35099b4efa4fdc1bed4025a10b50615
laurenpawell <lauren.pawell@gmail.com>
2015-02-23 23:22:21 -0800

    Edit index + show view for categories 2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,8 +8,9 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
-    @posts = @category.posts
+    category_id = params[:category_id]
+    @category = Category.find(params[:category_id])
+    @posts = @category.posts.order("posts.created_at DESC")
     add_breadcrumb "#{@category.category_name}", :category_path, :style => 'active'
   end
 end
